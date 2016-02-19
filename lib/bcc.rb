98f5317c042c4909130996218478892011a18a6d
John Otander <johnotander@gmail.com>
2016-02-19 16:41:51 -0700

    0.1.1

--- lib/bcc.rb
+++ lib/bcc.rb
@@ -4,7 +4,7 @@ require "bcc/railtie" if defined?(Rails::Railtie)
 module Bcc
   class Base
     def self.delivering_email(message)
-      return unless BCC_EMAILS.any?
+      return unless BCC_EMAILS.present?
       message.bcc ||= []
       message.bcc += BCC_EMAILS
     end
